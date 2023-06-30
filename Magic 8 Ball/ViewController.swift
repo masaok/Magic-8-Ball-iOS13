faf2120cae1c6c5fc63fc97ca219421489dd80da
Masao Kitamura <masaok@gmail.com>
2023-06-29 18:43:57 -0700

    Magic 8ball randomization done

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
+    
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+//        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
+//        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceSix")
+        
+    }
+    
 
-
+    @IBAction func askButtonPressed(_ sender: Any) {
+        
+        let randomIndex = Int.random(in: 0..<ballArray.count)
+        imageView.image = ballArray[randomIndex]
+    }
+    
 
 }
 
